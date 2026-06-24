76053a995bd79fa141ff874315bfc3ca3e247bb5
Jacek Kielich <jbackk@gmail.com>
2026-06-24 09:56:54 +0200

    Create smooth_audio function in easysound.py
    Add a basic audio smoothing function placeholder.

---
+++ src/easysound.py
@@ -0,0 +1,6 @@
+def smooth_audio(signal):
+    """
+    Podstawowa funkcja wygładzająca sygnał audio.
+    Wersja startowa – implementacja pojawi się w kolejnych krokach.
+    """
+    return signal
